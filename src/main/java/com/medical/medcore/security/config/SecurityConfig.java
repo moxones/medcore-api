b04db5c467c3a6c0b31c069ec15f9855c98a539c
Jose Antonio Arriola <moxones@gmail.com>
2026-06-05 23:09:45 -0500

    add subdomains

--- src/main/java/com/medical/medcore/security/config/SecurityConfig.java
+++ src/main/java/com/medical/medcore/security/config/SecurityConfig.java
@@ -62,7 +62,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:4200", "https://healthcare.pemedcore.xyz"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:4200",
+                "https://*.pemedcore.xyz"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         config.setAllowCredentials(true);
